5dd5546311ddb616581ce5c2f4c67c8697e7f669
Fermin Blanco <luillyfe89@gmail.com>
2024-09-22 02:11:16 -0500

    feat: preserving conversation history.

--- app/copilot/cohere.ts
+++ app/copilot/cohere.ts
@@ -3,19 +3,25 @@ import { CohereClient } from "cohere-ai";
 
 const token = process.env["COHERE_TOKEN"];
 
-console.log("token", token);
 const cohere = new CohereClient({
   token,
 });
 
-export async function askTheCopilot(userPrompt: string, category?: string): Promise<string> {
+export type ChatMessage = {
+  role: "USER" | "CHATBOT";
+  message: string;
+};
+
+let chatHistory: ChatMessage[] = [];
+
+export async function askTheCopilot(userPrompt: string, category?: string, ): Promise<string> {
   const stream = await cohere.chatStream({
     model: "command-r-plus",
     message: category 
-    ? `Por favor, proporciona información sobre la siguiente pregunta, centrándote en la categoría ${category} de las noticias de La Silla Vacía:\n\n${userPrompt}`
-    : userPrompt,
+      ? `Por favor, proporciona información sobre la siguiente pregunta, centrándote en la categoría ${category} de las noticias de La Silla Vacía:\n\n${userPrompt}`
+      : userPrompt,
     temperature: 0.3,
-    chatHistory: [],
+    chatHistory,
     promptTruncation: "AUTO",
     connectors: [{"id":"web-search","options":{"site":"https://www.lasillavacia.com/"}}]
   });
@@ -26,6 +32,10 @@ export async function askTheCopilot(userPrompt: string, category?: string): Prom
           response += chat.text;
       }
   }
+
+  // Add the new messages to the chat history
+  chatHistory.push({ role: "USER", message: userPrompt });
+  chatHistory.push({ role: "CHATBOT", message: response });
 
   return response;
 }
